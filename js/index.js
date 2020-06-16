4662603eab2723fff493aa94bbf782e848f059b4
Fabio <fabiog@live.com.pt>
2020-06-16 15:20:43 -0400

    created variables for form and messages

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 'use strict'
+
+//create a socket
+const socket = io()
+
+//send a message
+socket.emit('newuser', {user: 'Grace Hopper'}) //set a variable 'newuser' and the data
+
+// Event listener, waiting for an incoming "newuser"
+socket.on('newuser', (data) => console.log(`${data.user} has connected!`))
+
+const $form = document.getElementById('form')
+const $messages = document.getElementById('messages')
+const $submit_button =document.getElementById('submit_button')
+
