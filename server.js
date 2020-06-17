f7683f0527bcae6a8e8a28c94913a258c04dc283
Fabio <fabiog@live.com.pt>
2020-06-16 22:50:36 -0400

    added prompt user socket in server

---
+++ server.js
@@ -0,0 +1,38 @@
+const express  = require('express')
+const app      = express()
+const port     = 3000
+const server   = app.listen(port, () => {console.log(`Server running at http://localhost:${port}`)})
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+app.use(express.static('.'))
+
+//connect socket.io to servers, listen for socket.io on the port/server
+const io = require('socket.io').listen(server)
+
+io.on('connection', function (socket) {
+   
+   
+   socket.on('username',(data)=>{
+      socket.emit($userName,data)
+   })
+   // Listen for a "newuser" message
+   socket.on('newuser', (data) => {
+     // Transmit a message to everyone except the sender
+     socket.broadcast.emit('newuser', data)
+ 
+     // The same message, sent to all users - try it!
+     //io.emit('newuser', data)
+    })
+     
+    
+    // Listen for "chatmsg"
+    //   io.emit to all user
+    socket.on('chatmsg', (data) => {
+       io.emit('chatmsg', data)
+    })
+ 
+ 
+ })
