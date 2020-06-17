ff3f3b4ae18e923388c469b97c1202adcae4cb6b
Fabio <fabiog@live.com.pt>
2020-06-16 22:39:47 -0400

    add prompt for username on index,js

--- js/index.js
+++ js/index.js
@@ -3,13 +3,32 @@
 //create a socket
 const socket = io()
 
+//prompt for username
+const $userName = prompt('Please enter your username!')
+socket.emit('username',$userName)
 //send a message
-socket.emit('newuser', {user: 'Grace Hopper'}) //set a variable 'newuser' and the data
+socket.emit('newuser', {user: 'Fabio'}) //set a variable 'newuser' and the data
 
 // Event listener, waiting for an incoming "newuser"
 socket.on('newuser', (data) => console.log(`${data.user} has connected!`))
 
-const $form = document.getElementById('form')
+const $msgForm = document.getElementById('form')
 const $messages = document.getElementById('messages')
-const $submit_button =document.getElementById('submit_button')
+// const $submit_button =document.getElementById('submit_button')
+// const $text = document.getElementById('text')
 
+//listen for the submit of a form
+$msgForm.addEventListener('submit', (event) =>{
+    event.preventDefault()
+
+    socket.emit('chatmsg', {msg: event.currentTarget.text.value})
+    event.currentTarget.text.value = ''
+    })
+
+    socket.on('chatmsg', (data)=>{
+        const newMsg = document.createElement('li')
+        $messages.appendChild(newMsg)
+
+        newMsg.textContent = data.msg
+        
+    })
